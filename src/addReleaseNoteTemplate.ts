be6dabf031775f4ad8b4b533d19ab7caed1a5d61
MasatoMakino <makino.masato.g@gmail.com>
2024-11-30 12:00:01 +0900

    fix: update comments and logging for addReleaseNoteTemplate function

--- src/addReleaseNoteTemplate.ts
+++ src/addReleaseNoteTemplate.ts
@@ -4,20 +4,22 @@ import { resolve } from "node:path";
 const __dirname = import.meta.dirname;
 
 /**
- * copy template file "release.yml" to .github folder
+ * copy template file "release.yml" to .github directory
  * @param isForce overwrite existing file
  */
 export async function addReleaseNoteTemplate(isForce = false) {
 	try {
 		const from = resolve(__dirname, "../template/release.yml");
 		const to = resolve("./.github/release.yml");
-		console.log(from);
-		console.log(to);
 
 		await copyFile(from, to, isForce ? undefined : constants.COPYFILE_EXCL);
+
+		console.log("Copied release.yml file to .github/workflows directory");
 	} catch (e) {
 		if ((e as NodeJS.ErrnoException).code === "EEXIST") {
-			console.log("Skip copying release.yml file to .github/workflows folder");
+			console.log(
+				"Skip copying release.yml file to .github/workflows directory",
+			);
 			return;
 		}
 		throw e;
