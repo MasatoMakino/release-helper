b34f83b1d65ad3bd390188777c65d94e39d74b10
MasatoMakino <makino.masato.g@gmail.com>
2024-12-01 16:47:27 +0900

    Enhance pull request handling with error checking and new watchMerged functionality

--- src/isExecaError.ts
+++ src/isExecaError.ts
@@ -3,3 +3,12 @@ import type { ExecaError } from "execa";
 export function isExecaError(e: unknown): e is ExecaError {
 	return (e as ExecaError).name === "ExecaError";
 }
+
+export function isExecaErrorWithErrorCode(e: unknown, code: string): boolean {
+	return (
+		isExecaError(e) &&
+		e.stderr !== undefined &&
+		typeof e.stderr === "string" &&
+		e.stderr.includes(code)
+	);
+}
