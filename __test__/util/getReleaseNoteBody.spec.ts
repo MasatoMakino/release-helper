1c0a7971a46632e114e0552e737f71c2bd0e85ea
MasatoMakino <makino.masato.g@gmail.com>
2024-12-27 15:41:52 +0900

    feat: add getReleaseNoteBody function and corresponding tests

---
+++ __test__/util/getReleaseNoteBody.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it, vi } from "vitest";
+import { getReleaseNoteBody } from "../../src/util/getReleaseNoteBody.js";
+
+describe("getReleaseNoteBody", () => {
+	it("should generate the correct release note body", async () => {
+		vi.mock("execa", () => ({
+			execa: vi.fn().mockResolvedValue({
+				stderr: "",
+				stdout: JSON.stringify({
+					body: "## What's Changed",
+				}),
+				exitCode: 0,
+			}),
+		}));
+		const result = await getReleaseNoteBody("v0.1.1");
+		expect(result).toMatch("## What's Changed");
+
+		vi.restoreAllMocks();
+	});
+});
