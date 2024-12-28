61d51f8a341d267a5782982854451d27c1e2b95d
MasatoMakino <makino.masato.g@gmail.com>
2024-12-28 13:34:21 +0900

    feat: export additional utility functions from util module

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,1 +1,8 @@
 export { isExecaError, isExecaErrorWithErrorCode } from "./isExecaError.js";
+export {
+	getTagVersion,
+	getPreviousTagVersion,
+	getTagBranchName,
+} from "./getTagVersion.js";
+export { getReleaseNoteBody } from "./getReleaseNoteBody.js";
+export { wrapDependencies } from "./wrapDependencies.js";
