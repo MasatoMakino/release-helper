8dc919a3b3d8660ab77edebc3810bf17de20e85c
MasatoMakino <makino.masato.g@gmail.com>
2024-12-28 13:34:52 +0900

    feat: enhance addPullRequestLabel function documentation

--- src/addPullRequestLabel.ts
+++ src/addPullRequestLabel.ts
@@ -2,7 +2,17 @@ import { execa } from "execa";
 import { isExecaErrorWithErrorCode } from "./util/index.js";
 
 /**
- * initialize release label
+ * Adds or updates a label in the GitHub repository for a pull request.
+ *
+ * This function creates a label with the specified name, description, and color.
+ * If a label with the same name already exists, it updates the label based on the error message.
+ *
+ * @param label - The name of the label to create or update.
+ * @param description - The description of the label.
+ * @param color - The color of the label in hexadecimal format (e.g., "FF5733").
+ * @returns A promise that resolves when the label has been successfully created or updated.
+ *
+ * @throws {Error} If an error occurs while creating or updating the label.
  */
 export async function addPullRequestLabel(
 	label: string,
