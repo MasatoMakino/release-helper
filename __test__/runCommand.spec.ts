35770a3e7191c6eb9f57ad25f0b6cce08c80bf74
MasatoMakino <makino.masato.g@gmail.com>
2025-01-03 17:00:37 +0900

    test: add tests for preversion and postversion commands in runCommand

--- __test__/runCommand.spec.ts
+++ __test__/runCommand.spec.ts
@@ -1,12 +1,17 @@
 import { beforeEach, describe, expect, it, vi } from "vitest";
+import { postversion } from "../src/postversion.js";
+import { preversion } from "../src/preversion.js";
 import { runCommand } from "../src/runCommand.js";
+
+vi.mock("../src/preversion.js");
+vi.mock("../src/postversion.js");
 
 describe("runCommand", () => {
 	beforeEach(() => {
 		vi.restoreAllMocks();
 	});
 
-	it("should run the command with --help", async () => {
+	const mockFunctions = () => {
 		const stdOutSpy = vi
 			.spyOn(process.stdout, "write")
 			// @ts-ignore
@@ -15,6 +20,11 @@ describe("runCommand", () => {
 			.spyOn(process, "exit")
 			// @ts-ignore
 			.mockImplementation(() => {});
+		return { stdOutSpy, mockExit };
+	};
+
+	it("should run the command with --help", async () => {
+		const { stdOutSpy, mockExit } = mockFunctions();
 
 		process.argv = ["node", "cli.ts", "--help"];
 		runCommand();
@@ -23,5 +33,33 @@ describe("runCommand", () => {
 		expect(mockExit).toHaveBeenCalledWith(0);
 		stdOutSpy.mockRestore();
 		mockExit.mockRestore();
+	});
+
+	it("should run the command previersion", async () => {
+		const mockPreversion = vi.mocked(preversion).mockResolvedValue();
+
+		process.argv = ["node", "cli.ts", "preversion"];
+		runCommand();
+
+		expect(mockPreversion).toBeCalledWith({
+			defaultBranch: "main",
+			dryRun: false,
+			testCommand: "npm test",
+		});
+		mockPreversion.mockRestore();
+	});
+
+	it("should run the command postversion", async () => {
+		const mockPostversion = vi.mocked(postversion).mockResolvedValue();
+
+		process.argv = ["node", "cli.ts", "postversion"];
+		runCommand();
+
+		expect(mockPostversion).toBeCalledWith({
+			defaultBranch: "main",
+			dryRun: false,
+			useAutoMerge: true,
+		});
+		mockPostversion.mockRestore();
 	});
 });
