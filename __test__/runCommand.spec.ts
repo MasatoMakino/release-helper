85d03b544b3d739d3fdd10fe5595fac4c6203d4a
MasatoMakino <makino.masato.g@gmail.com>
2025-01-03 16:04:44 +0900

    feat: implement command runner and add tests for runCommand function

---
+++ __test__/runCommand.spec.ts
@@ -0,0 +1,27 @@
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { runCommand } from "../src/runCommand.js";
+
+describe("runCommand", () => {
+	beforeEach(() => {
+		vi.restoreAllMocks();
+	});
+
+	it("should run the command", async () => {
+		const stdOutSpy = vi
+			.spyOn(process.stdout, "write")
+			// @ts-ignore
+			.mockImplementation(() => {});
+		const mockExit = vi
+			.spyOn(process, "exit")
+			// @ts-ignore
+			.mockImplementation(() => {});
+
+		process.argv = ["node", "cli.ts", "--help"];
+		runCommand();
+		expect(stdOutSpy).toHaveBeenCalledWith(expect.stringContaining("Usage:"));
+
+		expect(mockExit).toHaveBeenCalledWith(0);
+		stdOutSpy.mockRestore();
+		mockExit.mockRestore();
+	});
+});
